7e6c4092897b07e5e9b4bd3a778aa58e110334d7
itdev-lenovo <it1devops@gmail.com>
2020-03-26 20:23:28 +0530

    files added

---
+++ Level3/Controllers/NewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Level3.Controllers
+{
+    public class NewController : Controller
+    {
+        public IActionResult Index()
+        {
+            //.Enable Paging, Sorting and Filtering to the data being rendered byGet Customers API (Created in previous steps)
+            return View();
+        }
+    }
+}
